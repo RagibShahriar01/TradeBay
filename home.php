74c278c7bf25c39d6c6a4be8b680a8826adf833f
opu02 <opu02@gmail.com>
2025-12-16 23:39:21 +0600

    half home page

---
+++ home.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>TradeBay</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="first-section">
+        <div class="a-container">
+        <div class="first-container">
+        Hi buddy! <a href="">Sign in</a> or <a href="">Register</a>
+        </div>
+
+        <div class="second-container">
+            
+            <a href="">Help & Contact</a>
+        </div>
+        <div class="second-container">
+        <a href="">Sell</a>
+    </div>
+    </div>
+
+    <div class="list-container">
+        <div>
+        <select class="dropdown">
+            <option>Watchlist</option>
+        </select>
+       </div>
+       <div>
+        <select class="dropdown">
+            <option>My TradeBay</option>
+        </select>
+       </div>
+       <div>
+         <a href="">
+            <img src="./images/first.png" alt="">
+        </a>
+        <a href="">
+            <img src="./images/second.png" alt="">
+        </a>
+       </div>
+    </div>
+    </section>
+
+    <hr>
+
+    <section class="second-section">
+        <div class="logo-container">
+            <a href="">
+                <span style="color: blue;">T</span>
+                 <span style="color: green;">r</span>
+                 <span style="color: red;">a</span>
+            <span style="color: blue;">d</span>
+            <span style="color: red;">e</span>
+            <span style="color: green;">B</span>
+            <span style="color: blue;">a</span>
+            <span style="color: orange;">y</span>
+            </a>
+        </div>
+         <form class="search-bar">
+        <div class="search-input">
+            <span class="search-icon">🔍</span>
+            <input type="text" placeholder="Search for anything">
+        </div>
+
+        <select class="search-category">
+            <option>All Categories</option>
+            <option>Electronics</option>
+            <option>Fashion</option>
+            <option>Books</option>
+        </select>
+
+        <button type="submit" class="search-btn">Search</button>
+    </form>
+    </section>
+
+    <hr>
+    <div class="category-bar">
+      <a href="">Electronic</a>
+      <a href="">Motors</a>
+      <a href="">Fashion</a>
+      <a href="">Collectibles & Art</a>
+      <a href="">Sports</a>
+      <a href="">Healthy & Beauty</a>
+      <a href="">Industrial equipment</a>
+      <a href="">Home & Garden</a>
+    </div>
+
+    <div class="third-section">
+        <img src="./images/fourth.jpg" alt="">
+    </div>
+
+    <section class="fourth-section">
+      <div class="fourth-container">
+        <div class="shopping">
+        <p id="p1">Shopping made easy</p>
+        <br>
+        <p id="p2">Enjoy reliability,secure deliveries and hassle-free returns</p>
+      </div>
+      <div>
+        <button type="submit" class="search-btn1">Start Now</button>
+      </div>
+      </div>
+    </section>
+
+    <section class="fifth-section">
+        <p id="p3">The future in your hands</p>
+        <div>
+            <div>
+                <a href=""><img src="./images/fifth.png" alt=""></a>
+                <a href="">Laptops</a>
+            </div>
+            <div>
+                <a href=""><img src="./images/sixth.png" alt=""></a>
+                <a href="">Computer Parts</a>
+            </div>
+            <div>
+                <a href=""><img src="./images/seventh.png" alt=""></a>
+                <a href="">Smartphones</a>
+            </div>
+            <div>
+                <a href=""><img src="./images/eighth.png" alt=""></a>
+                <a href="">Enterprise Networking</a>
+            </div>
+            <div>
+                <a href=""><img src="./images/ninth.png" alt=""></a>
+                <a href="">Tablets & eBooks</a>
+            </div>
+            <div>
+                <a href=""><img src="./images/tenth.png" alt=""></a>
+                <a href="">Storage & blank Media</a>
+            </div>
+            <div>
+                <a href=""><img src="./images/eleventh.png" alt=""></a>
+                <a href="">Lenses & filters</a>
+            </div>
+            
+        </div>
+    </section>
+
+</body>
+</html>
+
+
+
+
